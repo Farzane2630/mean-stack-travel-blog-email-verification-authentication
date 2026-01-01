32dff19f2168c1fb06e18b7d14251311547f264b
farzane <farzanekazemi8517@gmail.com>
2026-01-01 16:33:36 +0330

    setting up dependencies

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  API_AUTH_URL: "http://localhost:5000/api/auth", // Your API base URL for development
-  API_URL: "http://localhost:5000/api", // Your API base URL for development
-  IMAGE_BASE_URL: "http://localhost:5000", // Your AVATAR API 
+  API_AUTH_URL: "http://localhost:5000/api/auth", // API base URL for development
+  API_URL: "http://localhost:5000/api", // API base URL for development
+  IMAGE_BASE_URL: "http://localhost:5000", // AVATAR API 
 };
