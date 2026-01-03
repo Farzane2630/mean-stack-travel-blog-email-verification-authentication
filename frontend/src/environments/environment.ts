ed60cea23e9b514a57204925a3755599b4aa1d8a
farzane <farzanekazemi8517@gmail.com>
2026-01-03 12:57:36 +0330

    fix: api url

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  API_AUTH_URL: "http://localhost:5000/api/auth", // API base URL for development
-  API_URL: "http://localhost:5000/api", // API base URL for development
-  IMAGE_BASE_URL: "http://localhost:5000", // AVATAR API 
+  API_AUTH_URL: "https://mean-stack-travel-blog-email.onrender.com/api/auth", // API base URL for development
+  API_URL: "https://mean-stack-travel-blog-email.onrender.com/api", // API base URL for development
+  IMAGE_BASE_URL: "https://mean-stack-travel-blog-email.onrender.com", // AVATAR API 
 };
